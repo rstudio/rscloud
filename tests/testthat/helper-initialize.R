e8c0532b89afb397feb6cfb64b5575a0f8b78a09
kevinykuo <kevinykuo@gmail.com>
2019-09-24 14:57:20 -0700

    wrap pre-test cleanup in function

--- tests/testthat/helper-initialize.R
+++ tests/testthat/helper-initialize.R
@@ -1,13 +1,18 @@
 library(rscloud)
-# clean up
-space <- rscloud_space(name = "test-space-1")
 
-members <- space %>%
-  space_member_list() %>%
-  dplyr::filter(email != "kevin.kuo+rscloud@rstudio.com")
-space %>%
-  purrr::safely(space_member_remove)(members, ask = FALSE)
+clean_up <- function() {
+  space <- rscloud_space(name = "test-space-1")
 
-space %>%
-  purrr::possibly(space_invitation_list, otherwise =NULL)() %>%
-  purrr::safely(invitation_rescind)()
+  members <- space %>%
+    space_member_list() %>%
+    dplyr::filter(email != "kevin.kuo+rscloud@rstudio.com")
+  space %>%
+    purrr::safely(space_member_remove)(members, ask = FALSE)
+
+  space %>%
+    purrr::possibly(space_invitation_list, otherwise =NULL)() %>%
+    purrr::safely(invitation_rescind)()
+  invisible(NULL)
+}
+
+clean_up()
